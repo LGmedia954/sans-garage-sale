56a7779d85838415f46b66e97555aa189477bfb4
LGmedia954 <LeslieGonzalez@me.com>
2021-04-28 15:52:32 -0400

    validations to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,10 @@ class Item < ActiveRecord::Base
     belongs_to :user
     has_many :item_categories
     has_many :categories, through: :item_categories
+
+    validates :name, :presence => true
+    validates :quantity, :presence => true
+    validates :condition, :presence => true
+    validates :price, :presence => true
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
     has_many :categories, through: :items
     has_secure_password
 
+    validates :name, :presence => true
+    validates :email, :presence => true
+    validates :phone, :presence => true
     validates_uniqueness_of :email
     
 end
