7f455b80bc7f146eea7e7dbf0c8082022b2f6b17
LGmedia954 <LeslieGonzalez@me.com>
2021-04-28 14:41:39 -0400

    seed list

---
+++ db/seeds.rb
@@ -0,0 +1,40 @@
+
+
+category_list = {
+    "Appliances" => {
+    },
+    "Books" => {
+    },
+    "Collectibles" => {
+    },
+    "Computers" => {
+    }
+    "Electronics" => {
+    }
+    "Furniture" => {
+    }
+    "Games" => {
+    }
+    "Garden" => {
+    }
+    "Household" => {
+    }
+    "Kids" => {
+    },
+    "Media" => {
+    }
+    "Music" => {
+    }
+    "Sports" => {
+    }
+    "Tools" => {
+    }
+    "Vehicles" => {
+    }
+  }
+
+category_list.each do |name, category_hash|
+  c = Category.new
+  c.name = name
+  c.save
+end
