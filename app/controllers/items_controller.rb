a1d24d8e07f56d439a469a1ab36a2ace38ec9eee
LGmedia954 <LeslieGonzalez@me.com>
2021-04-28 16:44:26 -0400

    updates to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,22 +3,15 @@ require 'rack-flash'
 
 class ItemsController < ApplicationController
 
-    get '/listings' do
-      #Trying this code here...
-      #@venue = Venue.includes({:orders => [:customer, :items]}).find_by_handle(params[:venue])
-      @item = Item.includes({:items => [:name, :quantity, :condition, :price]}, {:users => [:name, :phone, :email]}).find_by_handle(params[:item])
-      erb :'/listings'
-    end
-
-    get '/add_listing' do
-      @categories = Category.all
-      erb :'/add_listing'
+    get '/item/new' do
+      @catergories = Category.all
+      erb :'/item/new'
     end
 
     post 'items' do
       if params[:name] == "" || params[:quantity] == "" || params[:condition] == "" || params[:price] == ""
         flash[:input_error] = "All fields are required. Enter 0 for free items."
-        redirect to '/add_listing'
+        redirect to '/item/new'
       else
         @item = Item.create(params)
 
@@ -27,16 +20,26 @@ class ItemsController < ApplicationController
           @item.categories << Category.find(category)
         end
 
+        @item.save
         redirect '/show_listing/#{@item.last}'
       end
 
     end
 
-    get '/show_listing' do
-      erb :'show_listing'
+    get '/listings' do
+      #Trying this code here...
+      #@venue = Venue.includes({:orders => [:customer, :items]}).find_by_handle(params[:venue])
+      @item = Item.includes({:items => [:name, :quantity, :condition, :price]}, {:users => [:name, :email, :phone]}).find_by_id(params[:item])
+      erb :'/listings'
     end
 
-    get '/edit_listing' do
+    get '/item/:name' do
+      name = params[:name]
+      @item = Item.find_by_name(name)
+      erb :'/show_listing'
+    end
+
+    get '/item/edit' do
       erb :'edit_listing'
     end
 
