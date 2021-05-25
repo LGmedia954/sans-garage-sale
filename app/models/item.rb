b06720ae353c21ed500c9712735a468da7544fc1
LGmedia954 <LeslieGonzalez@me.com>
2021-05-25 13:55:16 -0400

    added category_id to Item validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ActiveRecord::Base
     has_many :item_categories
     has_many :categories, through: :item_categories
 
-    validates_presence_of :name, :quantity, :condition, :price, message: "fields cannot be blank."
+    validates_presence_of :name, :quantity, :condition, :price, :category_id, message: "fields cannot be blank."
 
 
   
